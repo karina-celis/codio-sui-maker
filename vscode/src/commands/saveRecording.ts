91b179d130d1ee446bae0129815ea7d733776df4
Robert Brisita <rbrisita@hotmail.com>
2022-02-08 14:58:31 -0500

    Added better handling of saving modified files when a recording ends. CLoses #98.

--- vscode/src/commands/saveRecording.ts
+++ vscode/src/commands/saveRecording.ts
@@ -1,11 +1,21 @@
 import { UI, MODAL_MESSAGE_OBJS } from '../user_interface/messages';
 import Recorder from '../recorder/Recorder';
-import { workspace } from 'vscode';
+import { window, workspace } from 'vscode';
+import { schemeSupported } from '../utils';
 
 export default async function saveRecording(recorder: Recorder): Promise<void> {
   try {
     if (recorder && recorder.isRecording) {
-      await workspace.saveAll(true);
+      for (let i = 0; i < workspace.textDocuments.length; i++) {
+        const td = workspace.textDocuments[i];
+        if (!schemeSupported(td.uri.scheme)) {
+          continue;
+        }
+
+        await window.showTextDocument(td.uri, { preview: false });
+        await td.save();
+      }
+
       await recorder.stopRecording();
       recorder.saveRecording();
       UI.showModalMessage(MODAL_MESSAGE_OBJS.recordingSaved);
